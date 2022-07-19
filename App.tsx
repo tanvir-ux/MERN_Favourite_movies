479d36a06644be436bc73a60d8fc2223abf20020
tanvir-ux <tanviralamshuvo@gmail.com>
2022-07-20 00:47:47 +0600

    fetch random data from a public api using axios get method

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
+import axios from "axios";
 import './style.css';
+
+
+
 
 export default function App() {
   const {useState} = React;
@@ -9,10 +13,26 @@ export default function App() {
     setCounter(counter + 1);
   }
 
+  const url = 'https://randomuser.me/api/';
+
+  const fetchRandomData = () => {
+    axios.get(url)
+    .then((res) => {
+      // handle success
+      console.log(res);
+      return res;
+    })
+    .catch( (error) => {
+      // handle error
+      console.error(error);
+    })
+  }
+
   return (
     <div>
       <h1>Counter: {counter}</h1>
-      <button onClick={increase}>Increase Counter</button>
+      <button onClick={increase}>Increase Counter</button><br/>
+      <button onClick={fetchRandomData}>Fetch Random Data</button>
     </div>
   );
 }
